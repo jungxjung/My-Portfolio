d74074d841b6587646221879be8254680c7b287d
Chih-Jung Chen <jungxjung@gmail.com>
2021-06-08 22:26:43 -0400

    fix typo

--- js/jscript.js
+++ js/jscript.js
@@ -168,7 +168,7 @@ function loadContent() {
             var e = document.createElement('div');
             e.innerHTML = miscWorks[i].description;
             for (let k =0; k < e.children.length; k++){
-                console.log(e.children[0]);
+                console.log(e.children[k]);
                 $('.miscWork')[i].appendChild(e.children[k]);
                 
             }
