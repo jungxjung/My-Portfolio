97388269205db1d286fdec3da716762a7d127db0
Chih-Jung Chen <jungxjung@gmail.com>
2021-06-08 21:55:04 -0400

    bug fix

--- js/jscript.js
+++ js/jscript.js
@@ -160,9 +160,9 @@ function loadContent() {
         }
         $("#miscA")[0].innerHTML=data.project.misc.heading;
         var miscWorks = data.project.misc.miscWork;
-        $('.miscwork').remove('p');
-        $('.miscwork').remove('a');
-        $('.miscwork').remove('i');
+        $('.miscwork p').remove();
+        $('.miscwork i').remove();
+        $('.miscwork a').remove();
         for(let i=0; i < miscWorks.length; i++){
             $('.misc-title')[i].innerHTML = miscWorks[i].title;
             var e = document.createElement('div');
